157d56aa4917d662c240cee5fd1c66cb8f561eeb
Arbab Ahmed <Arbab.Ahmed@academytrainee.com>
2020-07-10 11:27:46 +0100

    created OrderController and implemented the interface CrudController

---
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import com.mysql.cj.x.protobuf.MysqlxCrud.Order;
+import com.qa.ims.persistence.domain.Product;
+
+public class OrderController implements CrudController<Order> {
+
+	@Override
+	public List<Order> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
